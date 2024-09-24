422db27d00116d2653d0d263b88c41da3c9d9455
CongAnh201003 <conganh2kk3@gmail.com>
2024-09-24 22:57:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import HeaderComponent from "./assets/Component/header/Header";
+import CourseComponent from "./assets/Component/course/Course";
+import AboutComponent from "./assets/Component/aboutUs/about";
+import SliderComponent from "./assets/Component/slider/Slider";
+import FooterComponent from "./assets/Component/footer/footer";
+import PartnerComponent from "./assets/Component/partner/partner";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="exo-font">
+      {" "}
+      <HeaderComponent />
+      <SliderComponent />
+      <AboutComponent/>
+      <CourseComponent />
+      <PartnerComponent/>
+      <FooterComponent />
+    </div>
+  );
+}
